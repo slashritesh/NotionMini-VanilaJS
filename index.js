228a81174bb08e643f0f8bb45a2306ec51ba68df
RITESH MANE <maneritesh@gmail.com>
2023-08-26 20:19:34 +0530

    trying to fix/Textarea,input

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 
-const textarea = document.querySelectorAll('textarea')
-const line = document.querySelector('.line')
-const wrapper = document.querySelector('.wrapper')
+const next = document.querySelector('.next')
+const next2 = document.querySelector('.next2')
 
 
 
-
-
-wrapper.addEventListener('keypress',function (e) {
-  let newLine = document.createElement('div')
-    if (e.key === 'Enter') {
-    e.preventDefault()
-    newLine.classList.add('line')
-    newLine.innerHTML = line.innerHTML
-    newLine.children[1].innerHTML = ""
-    wrapper.append(newLine)
-    newLine.removeAttribute('placeholder')
-    const input = newLine.querySelector('textarea')
-    input.focus()
-    input.style.height = ''
-    }
-})
-
-
-
-
-wrapper.addEventListener('keyup',function (e) {
-    e.preventDefault()
-    textarea.style.height = `28px`
-    let screenH = e.target.scrollHeight
-    textarea.style.height = `${screenH}px`
-})
-
-
-
+next2.focus()
 
 
 
